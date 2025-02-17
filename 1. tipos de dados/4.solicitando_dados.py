180a8adfcc62bce8b76e7e2507970e6bee410f3b
Christian77-max <christianbarros382@gmail.com>
2025-02-17 19:49:54 +0000

    estruturas condicionais

--- 1. tipos de dados/4.solicitando_dados.py
+++ 1. tipos de dados/4.solicitando_dados.py
@@ -3,8 +3,8 @@ os.system("clear")
 
 # Solicitando dados.
 nome = input("Digite seu nome: ")
-idade = int()"(digite sua idade: "
-altura = float(input("Digiti sua altura: "))
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
 
 print(nome)
 print(idade)
