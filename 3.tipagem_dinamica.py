907b6be32b042bdd0e815243ce34407f8b753f08
Christian77-max <christianbarros382@gmail.com>
2025-02-17 18:42:04 +0000

    salvando

---
+++ 3.tipagem_dinamica.py
@@ -0,0 +1,13 @@
+# Denifinição de tipos obragatória.
+
+nome: str # Eequivalente ao caracter
+idade: int # Equivalente ao tipo inteiro
+altura: float # Equivalente ao tipo real
+
+nome = "Marta"
+idade = 23 
+altura = 1,65
+
+print(nome)
+print(idade)
+print(altura)
---
+++ 4.solicitando_dados.py
@@ -0,0 +1,11 @@
+import os
+os.system("clear")
+
+# Solicitando dados.
+nome = input("Digite seu nome: ")
+idade = int()"(digite sua idade: "
+altura = float(input("Digiti sua altura: "))
+
+print(nome)
+print(idade)
+print(altura)
